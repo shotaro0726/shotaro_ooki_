fe522d34099a1416c0eed8599c0610b841ea87ec
shotaro0726 <shoutaro0726@gmail.com>
2020-03-15 23:01:48 +0900

    View Add

--- favorite_spot/favorite/urls.py
+++ favorite_spot/favorite/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import Favorite_Spot_View
 
 app_name = 'favorite'
 
 urlpatterns = [
-    
+    path('',Favorite_Spot_View.as_view(),name='favorite_spot'),
 ]
